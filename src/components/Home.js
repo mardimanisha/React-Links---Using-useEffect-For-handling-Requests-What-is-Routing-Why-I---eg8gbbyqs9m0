55e8be4eba7b54ff961b08d206aa59b2ce5972ed
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-02-01 22:35:17 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div id="home-div">
       <ul>
-        <li>
-          <h2 id="home-item">Home</h2>
-        </li>
-        <li>
-          <h2 id="topics-item">Topics</h2>
-        </li>
+        <Link to={"/topics"}>
+          <li>
+            <h2 id="home-item">Home</h2>
+          </li>
+
+          <li>
+            <h2 id="topics-item">Topics</h2>
+          </li>
+        </Link>
       </ul>
       <hr />
     </div>
