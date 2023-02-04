d729fdb84fe539a869c058f319aefc94fd557030
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-02-04 16:46:20 +0530

    Update Topics.js

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,26 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link , useHistory, useNavigate } from "react-router-dom";
 const Topics = () => {
+  const history = useHistory();
   return (
     <div id="topics-div">
       <h2>Topics</h2>
       <ul>
         <li>
-          <Link to={"/topics/rendering"}>
-            <h2 id="rendering">Rendering</h2>
-          </Link>
+          <Link to="/topics/rendering"><h2 id="rendering">Rendering</h2></Link>
         </li>
         <li>
-          <Link to={"/topics/components"}>
-            <h2 id="components">Components</h2>
-          </Link>
+          <Link to="/topics/components"><h2 id="components">Components</h2></Link>
         </li>
       </ul>
       <hr />
-
-      <button id="topics-back-button">
-        <Link to={"/"}>Back</Link>
-      </button>
+      <button onClick={()=> history.goBack()} id="topics-back-button">Back</button>
     </div>
   );
 };
